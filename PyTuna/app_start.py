73d624a77f22a42e8241b607a10565ca36814929
Daniel Dutton <danielbdutton@hotmail.co.uk>
2015-02-23 11:42:19 +0000

    Added 6 string guitar wav resources. Tinkering with sound player

--- PyTuna/app_start.py
+++ PyTuna/app_start.py
@@ -3,10 +3,13 @@ from PyQt4.QtGui import *
 import sys
 from audio import notes
 from gui import console
+from audio import sound_player
 
 
 def main():
-    cons = console.Console()
+    #cons = console.Console()
+    sound = sound_player.SoundPlayer()
+    sound.play_sound(None)
 
 if __name__ == "__main__":
     main()
--- PyTuna/audio/sound_player.py
+++ PyTuna/audio/sound_player.py
@@ -1,11 +1,18 @@
 from winsound import PlaySound
-import os;
+import winsound
+import os
+from os import path
+
 
 class SoundPlayer(object):
 
-    def play_sound(filePath):
-        os.path()
-        PlaySound(r"C:\Users\Dan\Downloads\sound.wav", 0)
+    def play_sound(self, filePath):
+        #if path.exists(""):
+            p = path.realpath(r"resources\Guitar_A.wav") 
+            r = path.relpath(r"resources\Guitar_A")
+            winsound.PlaySound('%s.wav' % r"resources\Guitar_A.wav", winsound.SND_FILENAME)
+            PlaySound(p, 1)
+        
 
 
 
